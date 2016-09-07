24652b652e84132e7634fc0f20a471f1f1f145d0
Micaela <micaela.pereyra@hotmail.com>
2016-09-07 10:34:54 -0300

    Cthulhuuu

--- export/windows/neko/haxe/ApplicationMain.hx
+++ export/windows/neko/haxe/ApplicationMain.hx
@@ -511,7 +511,7 @@ class ApplicationMain {
 		
 		config = {
 			
-			build: "9",
+			build: "10",
 			company: "HaxeFlixel",
 			file: "Clase 04",
 			fps: 60,
---
+++ source/sprites/Enemy.hx
@@ -0,0 +1,25 @@
+package sprites;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * ...
+ * @author ...
+ */
+class Enemy extends FlxSprite
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		makeGraphic(16, 16);
+		velocity.
+		
+		override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		
+	}
+	}
+}
